22e2be25d1d7b5d8eaedcdf489e7e817073be96d
viraj-s15 <veer15102003@gmail.com>
2023-07-14 00:07:57 +0530

    feat: finished ui unit tests

--- root/frontend/__tests__/Form.test.tsx
+++ root/frontend/__tests__/Form.test.tsx
@@ -5,6 +5,15 @@ import Form from '../src/app/section/Form';
 test('renders the form UI', () => {
   render(<Form />);
   screen.getByText(/do you want your feedback to be anonymous\?/i)
+  screen.getByDisplayValue(/no/i)
+  screen.getByText(/name:/i)
+  screen.getByText(/registration number:/i)
+  screen.getByText(/email id:/i)
+  screen.getByText(/year of graduation:/i)
+  screen.getByDisplayValue(/2024/i)
+  screen.getByText(/problem subject/i)
+  screen.getByDisplayValue(/select/i)
+  screen.getByText(/describe your issue/i)
   screen.getByText(/year of graduation:/i)
   screen.getByText(/problem subject/i)
   screen.getByText(/describe your issue/i)
